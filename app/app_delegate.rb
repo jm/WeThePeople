f5402700e70a5adcd5b9e382998e850854bef456
Jeremy McAnally <jmcanally@github.com>
2013-02-23 16:31:33 -0500

    More comments and whitespace

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -1,15 +1,18 @@
 class AppDelegate
-  attr_reader :nav_controller
+  attr_reader :navController
 
   def application(application, didFinishLaunchingWithOptions:launchOptions)    
     WeThePeople::Config.api_key = ""
 
-    # Nav controllers are dumb.
+    # Nav controllers are dumb.  If we don't do this then there's a 
+    # gap at the top of the frame.
     @window = UIWindow.alloc.initWithFrame(CGRectOffset(UIScreen.mainScreen.applicationFrame, 0.0, -20.0))
+
     petitionsController = PetitionsController.alloc.initWithStyle(UITableViewStylePlain)
 
     @navController = UINavigationController.alloc.initWithRootViewController(petitionsController)
     @navController.navigationBar.tintColor = UIColor.colorWithRed(0.1, green:0.2, blue:0.6, alpha:0.3)
+
     petitionsController.navController = @navController
 
     @window.rootViewController = navController
